cd29f9b820474d408ea142c4b9cc077ab9843c7b
Fredrik Gummus <gamwed@gmail.com>
2016-04-07 17:50:46 +0200

    Added some initial design for the driver interface.

---
+++ Moridge/Models/DriverModels.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moridge.Models
+{
+    public class DriverModel
+    {
+        public string[] Days = { "Måndag", "Tisdag" };
+
+        [Display(Name = "vad är detta")]
+        public string Test = "testasd";
+    }
+}
