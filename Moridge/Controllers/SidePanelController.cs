74b7055573a44b61c3138a7e6457366523e6684c
Fredrik Gummus <gamwed@gmail.com>
2016-05-04 09:39:45 +0200

    Give the sidepanel's partial view a controller&model to be able to show personal info.

---
+++ Moridge/Controllers/SidePanelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Moridge.BusinessLogic;
+using Moridge.Models;
+
+namespace Moridge.Controllers
+{
+    [Authorize]
+    public class SidePanelController : Controller
+    {
+        [ChildActionOnly]
+        public ActionResult _SidePanelLoggedIn()
+        {
+            var model = new SidePanelModel();
+            return PartialView(model);
+        }
+
+        [AllowAnonymous]
+        [ChildActionOnly]
+        public ActionResult _SidePanelDefault()
+        {
+            var model = new SidePanelModel();
+            return PartialView(model);
+        }
+    }
+}
