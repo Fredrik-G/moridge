832d3a0eb114aa0222c1c1cf72be924be23c9a4c
Fredrik Gummus <gamwed@gmail.com>
2016-05-11 15:07:44 +0200

    Create basic interface for admin's interface for controlling drivers (listing all/personal details/creating new).

--- Moridge/Controllers/AdminController.cs
+++ Moridge/Controllers/AdminController.cs
@@ -10,15 +10,54 @@ namespace Moridge.Controllers
     {
         public ActionResult DriverRegister()
         {
-            var dbHandler = new DatabaseHelper();
             var drivers = System.Web.HttpContext.Current.Session["Drivers"] as List<ApplicationUser>;
             if (drivers == null)
             {
-                drivers = dbHandler.FindAllUsersInRole(RolesHelper.DriverRoleId);
+                drivers = new DatabaseHelper().FindAllUsersInRole(RolesHelper.DriverRoleId);
                 System.Web.HttpContext.Current.Session["Drivers"] = drivers;
             }
             var model = new DriverRegisterModel { Drivers = drivers };
             return View(model);
+        }
+
+        //
+        // GET: /Admin/DriverDetails
+        [HttpGet]
+        public ActionResult DriverDetails(int index)
+        {
+            var drivers = System.Web.HttpContext.Current.Session["Drivers"] as List<ApplicationUser>;
+            if (drivers == null)
+            {
+                drivers = new DatabaseHelper().FindAllUsersInRole(RolesHelper.DriverRoleId);
+                System.Web.HttpContext.Current.Session["Drivers"] = drivers;
+            }
+            var driver = drivers[index];
+            return View(new DriverDetailsModel { Driver = driver });
+        }
+
+        //
+        // POST: /Admin/DriverDetails
+        [HttpPost]
+        public ActionResult DriverDetails(DriverDetailsModel model)
+        {
+            return View(model);
+        }
+
+        //
+        // GET: /Admin/DriverCreate
+        [HttpGet]
+        public ActionResult DriverCreate()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Admin/DriverCreate
+        [HttpPost]
+        public ActionResult DriverCreate(DriverDetailsModel model)
+        {
+            //create user
+            return RedirectToAction("DriverRegister", "Admin");
         }
     }
 }
--- Moridge/Models/AdminModels.cs
+++ Moridge/Models/AdminModels.cs
@@ -1,9 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Moridge.Models
 {
     public class DriverRegisterModel
     {
         public List<ApplicationUser> Drivers { get; set; } 
+    }
+
+    public class DriverDetailsModel
+    {
+        public ApplicationUser Driver { get; set; }
+         
+        [Display(Name="Förnamn")]
+        public string FirstName => Driver.FirstName;
+
+        [Display(Name="Efternamn")]
+        public string LastName => Driver.LastName;
+
+        [Display(Name="E-post")]
+        public string Email => Driver.Email;
+
+        [Display(Name="Adress")]
+        public string Adress => Driver.Adress;
+
+        [Display(Name="Telefonnummer")]
+        public string PhoneNumber => Driver.PhoneNumber;
     }
 }
