494a4d9f1636d3faa5a12a705e7dfd614fc639a0
Fredrik Gummus <gamwed@gmail.com>
2016-05-16 13:17:12 +0200

    Add new floating button for easier access to creating a new booking.

---
+++ Moridge/Controllers/FloatingActionButtonController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Moridge.BusinessLogic;
+
+namespace Moridge.Controllers
+{
+    public class FloatingActionButtonController : Controller
+    {
+        [ChildActionOnly]
+        [AllowAnonymous]
+        public ActionResult _FloatingActionButton()
+        {
+            var user = UserHelper.GetCurrentUser();
+            return user.Role.Equals(RolesHelper.DriverRole) ? PartialView() : null;
+        }
+    }
+}
