981d4e961939bbbb7e8005deff41635382d86251
Fredrik Gummus <gamwed@gmail.com>
2016-05-17 10:08:37 +0200

    Add missing comment.

--- Moridge/BusinessLogic/Logger.cs
+++ Moridge/BusinessLogic/Logger.cs
@@ -4,6 +4,11 @@ namespace Moridge.BusinessLogic
 {
     public class Logger
     {
+        /// <summary>
+        /// Adds the given error to the database errorlog.
+        /// Also adds the current user's ID to the message.
+        /// </summary>
+        /// <param name="message">error message</param>
         public static void LogError(string message)
         {
             if (!HttpContext.Current.IsDebuggingEnabled)
